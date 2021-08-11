94c4f1fb09d44975f940ff510bb48e08c60f0d1b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-11 12:20:25 +0200

    added make file for arm32_asm version, removed a branch from assembly code

--- arm32_asm/collatz.s
+++ arm32_asm/collatz.s
@@ -1,13 +1,39 @@
 .text
 .global _start
 
+/*
+
+	An implementation of the collatz sequence.
+
+*/
+
 _start:
 	/* load start number into r6 */
 	mov r6, #27
 
 _collatz:
 	/* print hex value of current number */
-	b _format_hex
+_format_hex:
+        /* set up nibble mask in r3, shift amount + loop counter in r10 */
+        mov r3, #0xF0000000
+        mov r9, #28
+_format_hex_loop:
+        /* extract nibble from r6, shift result to right */
+        and r4, r3, r6
+        mov r4, r4, lsr r9
+
+	/* right-shift nibble mask for next iteration */
+        mov r3, r3, lsr #4
+
+        /* load address to character set, add value of nibble to get character, print it */
+        ldr r1, =chars
+        add r1, r1, r4
+        bl _printchar
+
+        /* decrement shift amount (r9), continue loop if result was positive (>=0) */
+        subs r9, r9, #4
+        bpl _format_hex_loop
+
 _collatz_continue:
 	/* print new line */
 	ldr r1, =new_line
@@ -28,29 +54,6 @@ _collatz_continue:
 	moveq r6, r6, lsr #1
 
 	b _collatz
-
-_format_hex:
-	/* set up nibble mask in r3, shift amount + loop counter in r10 */
-	mov r3, #0xF0000000
-	mov r9, #28
-_format_hex_loop:
-	/* extract nibble from r6, shift result to right */
-	and r4, r3, r6
-	mov r4, r4, lsr r9
-
-	/* load address to character set, add value of nibble to get character, print it */
-	ldr r1, =chars
-	add r1, r1, r4
-	bl _printchar
-
-	/* decrement shift amount (r9), jump back to collatz if result was negative */
-	subs r9, r9, #4
-	bmi _collatz_continue
-
-	/* right-shift nibble mask */
-	mov r3, r3, lsr #4
-
-	b _format_hex_loop
 
 _printchar:
 	mov r7, #4
