6cd2a7b68075c7f47d96f4dd110d72f9bb1a17ff
BuildTools <unconfigured@null.spigotmc.org>
2021-09-04 12:24:00 +0200

    added list_max func

--- haskell/Collatz.hs
+++ haskell/Collatz.hs
@@ -11,3 +11,18 @@ collatz_step x | x <= 1     = 1           -- the minimum value is 1
 collatz :: Integer -> [Integer]
 collatz x | x <= 1     = [1]
           | otherwise  = x : collatz (collatz_step x)
+
+-- calculates the start values for collatz sequences up to a number m
+--collatz_bruteforce :: Integer -> Integer -> (Integer, Integer) -- start v, maximum start v, returns (v for max seq "height", v for max seq len)
+--collatz_bruteforce v m | v < 1        = error "invalid start value. must be >= 1.
+--                       | v < m        = if collatz 
+
+
+
+
+
+-- calculates the maximum value in the list
+list_max :: Ord a => [a] -> a
+list_max [] = error "Empty list!"
+list_max [x] = x
+list_max (x:xs) = max x (list_max xs)
